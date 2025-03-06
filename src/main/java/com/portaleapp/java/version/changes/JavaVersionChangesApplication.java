a9e2ede55f834b638c352724e2b5e4f620ac043e
JavaSoftwareEntwickler <mm.java.software@gmail.com>
2025-03-06 14:23:31 +0100

    first commit

---
+++ src/main/java/com/portaleapp/java/version/changes/JavaVersionChangesApplication.java
@@ -0,0 +1,13 @@
+package com.portaleapp.java.version.changes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaVersionChangesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaVersionChangesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/portaleapp/java/version/changes/JavaVersionChangesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portaleapp.java.version.changes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaVersionChangesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
